d9ed02162956b2e94916fd699bd909abfe07ac82
Relun <claudia.araya.ro@gmail.com>
2019-03-15 11:50:59 -0300

    confirmando test de filtrado

--- src/main.js
+++ src/main.js
@@ -7,12 +7,12 @@ const showData = (data) => {
   // nombre y foto
   data.forEach(element => {
     // data[i]
-    container.innerHTML += `<div class="card" style="width: 10rem;">
+    container.innerHTML += `<div class="card" style="width: 12rem;">
     <img src="${element.img}" class="card-img-top" alt="${element.name}">
     <div class="card-body">
-    <h5 class="card-title">${element.name}</h5>
-    <p class="card-text">${element.num}</p>
-    <p class="card-text">${element.type}</p>
+    <h5 class="card-title">Nombre: ${element.name}</h5>
+    <p class="card-text">Número: ${element.num}</p>
+    <p class="card-text">Tipo: ${element.type}</p>
     </div>
 </div> `
   })
@@ -25,12 +25,12 @@ document.getElementById('pokefilter').addEventListener('change', () => {
   container.innerHTML = '';
   result.forEach(element => {
     container.innerHTML += `
-    <div class="card" style="width: 10rem;">
+    <div class="card" style="width: 12rem;">
       <img src="${element.img}" class="card-img-top" alt="${element.name}">
       <div class="card-body">
-      <h5 class="card-title">${element.name}</h5>
-      <p class="card-text">${element.num}</p>
-      <p class="card-text">${element.type}</p>
+      <h5 class="card-title">Nombre: ${element.name}</h5>
+      <p class="card-text">Número: ${element.num}</p>
+      <p class="card-text">Tipo: ${element.type}</p>
       </div>
 </div> `
 
@@ -38,8 +38,9 @@ document.getElementById('pokefilter').addEventListener('change', () => {
 
 let estadistica = result.length;
 let resultEst = window.computeStats(estadistica);
+
 let result2 = document.getElementById('estadistic');
- result2.innerHTML += `<p class="card-text">${resultEst.toFixed(2)}% del total de Pokémones son de este tipo</p>
+ result2.innerHTML = `<p class="card-text">${resultEst.toFixed(2)}% del total de Pokémones son de este tipo</p>
  <p class="card-text">${estadistica} Pokémones son de este tipo</p>`
 
 //console.log(resultEst.toFixed(2))
--- test/data.spec.js
+++ test/data.spec.js
@@ -3,30 +3,24 @@ global.assert = require('chai').assert;
 require('../src/data');
 require('./data.spec.js');
 
-describe('Funcion filterData', () => { // a quien voy a testear//
-  const data = [
-    {
-  
-      name: "Squirtle",
-      type: "Water"
-    
-    },
-    {
-      name:"Dratini",
-      type:"Dragon"
+const data = [
+  {
+    name: "Caterpie",
+    type: "Bug"
+  },
+  {
+    name:"Charmander",
+    type:"Fire"
+  }
+]
 
-    }
-  ]
-  
+describe('Funcion filterData', () => { // a quien voy a testear//
+ 
   it('debería ser una función', () => { // que voy a testear o probar//
     assert.deepEqual ( typeof filterData, 'function'); //assert: es quien se encarga de evaluar que el test haga lo que tiene que hacer, metodos son una funcion, ejem deepequal, esa funcion la entrega chai, en chai se ven ejemplos//
   });
 
-  it('deberia retornar el objeto "Squirtle" para primer pokemon tipo "Water"', () => {
-    assert.deepEqual(window.filterData(data, "Water"), [{name:"Squirtle", type: "Water"}]);
+  it('deberia retornar el objeto "Caterpie" para primer pokemon tipo "Bug"', () => {
+    assert.deepEqual(window.filterData(data, "Bug"), [{name:"Caterpie", type: "Bug"}]);
   });
 })
-
-  
-
-
